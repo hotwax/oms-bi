f26eeb4cd91b8357b35a5072bf5b45f87c1cf8e2
Purushottam Khedre <purushottamkhedre123@gmail.com>
2025-01-24 17:00:54 +0530

    Add enumeration sql

---
+++ sql/EnumerationDimension.sql
@@ -0,0 +1,8 @@
+SELECT
+  e.ENUM_ID enumId,
+  e.ENUM_TYPE_ID enumTypeId,
+  e.ENUM_CODE enumCode,
+  e.DESCRIPTION description,
+  e.ENUM_NAME enumName,
+  e.CREATED_TX_STAMP cursorDate
+FROM enumeration e
