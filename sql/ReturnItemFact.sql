21b4e7e045129d72ec000bcfb7ce544dafd0a192
Purushottam Khedre <purushottamkhedre123@gmail.com>
2025-02-14 18:45:03 +0530

    Update return item fact with order details

--- sql/ReturnItemFact.sql
+++ sql/ReturnItemFact.sql
@@ -6,6 +6,7 @@ SELECT
   ri.RETURN_ITEM_TYPE_ID returnItemTypeId,
   rh.RETURN_HEADER_TYPE_ID returnTypeId,
   DATE_FORMAT(rh.RETURN_DATE, "%Y-%m-%d %H:%i:%s") returnDate,
+  DATE_FORMAT(rh.ENTRY_DATE, "%Y-%m-%d %H:%i:%s") returnEntryDate,
   rh.STATUS_ID returnStatusId,
   ri.STATUS_ID returnItemStatusId,
   rh.DESTINATION_FACILITY_ID destinationFacilityId,
@@ -62,6 +63,24 @@ SELECT
   DATE_FORMAT(rs.STATUS_DATETIME, "%Y-%m-%d %H:%i:%s") returnItemCompletedDate,
   DATE_FORMAT(rs1.STATUS_DATETIME, "%Y-%m-%d %H:%i:%s") returnItemReceivedDate,
   rs1.CHANGE_BY_USER_LOGIN_ID receivedByUserLogin,
+  oh.ORDER_TYPE_ID AS orderTypeId,
+  oh.ORDER_NAME AS orderName,
+  oh.EXTERNAL_ID AS externalId,
+  oh.SALES_CHANNEL_ENUM_ID AS salesChannelEnumId,
+  DATE_FORMAT(oh.ORDER_DATE, "%Y-%m-%d %H:%i:%s") AS orderDate,
+  DATE_FORMAT(oh.ENTRY_DATE, "%Y-%m-%d %H:%i:%s") AS ordEntryDate,
+  oh.PRIORITY AS priority,
+  oh.CURRENCY_UOM AS currencyUomId,
+  oh.ORIGIN_FACILITY_ID AS orderOriginFacilityId,
+  oh.PRODUCT_STORE_ID AS productStoreId,
+  oh.GRAND_TOTAL AS grandTotal,
+  pa.CITY AS orderOrgCity,
+  pa.POSTAL_CODE AS orderOrgPostalCode,
+  pa.COUNTRY_GEO_ID AS orderOrgCountryGeoId,
+  pa.STATE_PROVINCE_GEO_ID AS orderOrgStateProvinceGeoId,
+  pa.MUNICIPALITY_GEO_ID AS orderOrgMunicipalityGeoId,
+  pa.LONGITUDE AS orderOrgLongitude,
+  pa.LATITUDE AS orderOrgLatitude,
   rs.CREATED_TX_STAMP cursorDate
 FROM return_item ri
 JOIN return_header rh ON rh.RETURN_ID = ri.RETURN_ID
@@ -69,6 +88,14 @@ JOIN return_status rs ON rs.RETURN_ID = ri.RETURN_ID
     AND rs.RETURN_ITEM_SEQ_ID = ri.RETURN_ITEM_SEQ_ID
     AND ri.STATUS_ID = rs.STATUS_ID
     AND ri.STATUS_ID = "RETURN_COMPLETED"
+LEFT JOIN order_header oh ON oh.ORDER_ID = ri.ORDER_ID
+LEFT JOIN (
+  select ocm.order_id, ocm.CONTACT_MECH_ID
+  FROM order_contact_mech ocm
+  WHERE ocm.CONTACT_MECH_PURPOSE_TYPE_ID = "SHIPPING_LOCATION"
+  group by ocm.order_id
+) ocm on ocm.order_id = ri.order_id
+LEFT JOIN postal_address pa ON pa.CONTACT_MECH_ID = ocm.CONTACT_MECH_ID
 LEFT JOIN return_status rs1 ON rs1.RETURN_ID = ri.RETURN_ID
     AND rs1.RETURN_ITEM_SEQ_ID = ri.RETURN_ITEM_SEQ_ID
     AND rs1.STATUS_ID = "RETURN_RECEIVED"
