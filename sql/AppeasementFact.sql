dd9d7bf8dd7ae5c2afd2932b45fce15443793d8b
Purushottam Khedre <purushottamkhedre123@gmail.com>
2025-02-14 19:31:15 +0530

    Add order details in appeasemet

--- sql/AppeasementFact.sql
+++ sql/AppeasementFact.sql
@@ -9,6 +9,7 @@ SELECT
   ) externalReturnId,
   "SHOPIFY" dataSourceId,
   DATE_FORMAT(rh.RETURN_DATE, "%Y-%m-%d %H:%i:%s") returnDate,
+  DATE_FORMAT(rh.ENTRY_DATE, "%Y-%m-%d %H:%i:%s") returnEntryDate,
   ra.ORDER_ID orderId,
   ra.COMMENTS comments,
   ra.DESCRIPTION description,
@@ -27,7 +28,32 @@ SELECT
       AND ris.RETURN_ITEM_SEQ_ID = "_NA_"
       AND opp.STATUS_ID = "PAYMENT_REFUNDED"
   ) returnRefundedTotal,
+  oh.ORDER_TYPE_ID AS orderTypeId,
+  oh.ORDER_NAME AS orderName,
+  oh.EXTERNAL_ID AS externalId,
+  oh.SALES_CHANNEL_ENUM_ID AS salesChannelEnumId,
+  DATE_FORMAT(oh.ORDER_DATE, "%Y-%m-%d %H:%i:%s") AS orderDate,
+  DATE_FORMAT(oh.ENTRY_DATE, "%Y-%m-%d %H:%i:%s") AS orderEntryDate,
+  oh.PRIORITY AS priority,
+  oh.CURRENCY_UOM AS currencyUomId,
+  oh.ORIGIN_FACILITY_ID AS orderOriginFacilityId,
+  oh.PRODUCT_STORE_ID AS productStoreId,
+  oh.GRAND_TOTAL AS grandTotal,
+  pa.CITY AS orderOrgCity,
+  pa.POSTAL_CODE AS orderOrgPostalCode,
+  pa.COUNTRY_GEO_ID AS orderOrgCountryGeoId,
+  pa.STATE_PROVINCE_GEO_ID AS orderOrgStateProvinceGeoId,
+  pa.MUNICIPALITY_GEO_ID AS orderOrgMunicipalityGeoId,
+  pa.LONGITUDE AS orderOrgLongitude,
+  pa.LATITUDE AS orderOrgLatitude,
   rh.CREATED_TX_STAMP cursorDate
 FROM return_header rh
-JOIN return_adjustment ra ON ra.RETURN_ID = rh.RETURN_ID
-    AND ra.RETURN_ADJUSTMENT_TYPE_ID = "APPEASEMENT"
+JOIN return_adjustment ra ON ra.RETURN_ID = rh.RETURN_ID AND ra.RETURN_ADJUSTMENT_TYPE_ID = "APPEASEMENT"
+LEFT JOIN order_header oh ON oh.ORDER_ID = ra.ORDER_ID
+LEFT JOIN (
+  select ocm.order_id, ocm.CONTACT_MECH_ID
+  FROM order_contact_mech ocm
+  WHERE ocm.CONTACT_MECH_PURPOSE_TYPE_ID = "SHIPPING_LOCATION"
+  group by ocm.order_id
+) ocm1 on ocm1.order_id = oh.order_id
+LEFT JOIN postal_address pa ON pa.CONTACT_MECH_ID = ocm1.CONTACT_MECH_ID
