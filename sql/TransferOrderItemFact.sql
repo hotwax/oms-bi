1700a6a7ff2cddccbd1051ba869a4c6f666e5126
Arvind <arvind.tomar@hotwaxsystems.com>
2025-11-03 15:04:44 +0530

    Updated the fetch and Join queries with optimized and working ones.

--- sql/TransferOrderItemFact.sql
+++ sql/TransferOrderItemFact.sql
@@ -1,5 +1,5 @@
 -- ******************************************************************
--- Base Query that fetches initial data
+-- Base Query 1 that fetches Initial data
 -- ******************************************************************
 WITH order_item_status as (
     SELECT
@@ -9,19 +9,8 @@ WITH order_item_status as (
       os.status_user_login,
       os.status_datetime
     FROM order_header oh
-      JOIN order_item oi ON oh.order_id = oi.ORDER_ID and oh.order_type_id = 'TRANSFER_ORDER'
+      JOIN order_item oi ON oh.order_type_id = 'TRANSFER_ORDER' and oh.status_id in ('ORDER_COMPLETED', 'ORDER_CANCELLED') and oh.order_id = oi.ORDER_ID and oi.status_id in ('ITEM_COMPLETED', 'ITEM_CANCELLED')
       JOIN order_status os ON oi.ORDER_ID = os.ORDER_ID AND oi.ORDER_ITEM_SEQ_ID = os.ORDER_ITEM_SEQ_ID and os.status_id in ('ITEM_COMPLETED', 'ITEM_CANCELLED')
-),
-manual_receipts AS (
-    SELECT
-        sr.ORDER_ID,
-        sr.PRODUCT_ID,
-        SUM(sr.QUANTITY_ACCEPTED) AS received_qty,
-        MAX(sr.datetime_received) AS received_date
-    FROM SHIPMENT_RECEIPT sr
-    WHERE sr.ORDER_ITEM_SEQ_ID IS NULL
-      AND sr.ORDER_ID IS NOT NULL
-    GROUP BY sr.ORDER_ID, sr.PRODUCT_ID
 )
 SELECT
   oh.order_id as `ORDER_ID`,
@@ -47,7 +36,7 @@ SELECT
   oi.status_id as `ORDER_ITEM_STATUS_ID`,
   CASE
         WHEN ois.status_id = 'ITEM_COMPLETED' THEN ois.status_datetime
-  END AS `ORDER_ITEM_COMPETION_DATE`,
+  END AS `ORDER_ITEM_COMPLETION_DATE`,
   CASE
         WHEN ois.status_id = 'ITEM_CANCELLED' THEN ois.status_datetime
   END AS `ORDER_ITEM_CANCELLATION_DATE`,
@@ -63,20 +52,32 @@ SELECT
   oi.cancel_quantity as `CANCEL_QUANTITY`,
   oisg.facility_id as `ORIGIN_FACILITY_ID`,
   oisg.order_facility_id as `DESTINATION_FACILITY_ID`,
+  date_format(oh.entry_date, "%Y-%m-%d %H:%i:%s.%f") AS `cursorDateStrFormatted`,
   os.status_datetime as `cursorDate`
 FROM
   order_header oh
   JOIN order_item oi ON oh.order_id = oi.order_id and oh.order_type_id = 'TRANSFER_ORDER' and oh.status_id IN ('ORDER_COMPLETED', 'ORDER_CANCELLED')
   JOIN order_status os ON oh.order_id = os.order_id and os.status_id in ('ORDER_COMPLETED', 'ORDER_CANCELLED')
   JOIN order_item_status ois ON oh.order_id = ois.order_id and oi.order_item_seq_id = ois.order_item_seq_id
-  LEFT JOIN order_shipment osh on oh.order_id = osh.order_id and oi.order_item_seq_id = osh.order_item_seq_id
+  LEFT JOIN order_shipment osh on oh.order_id = osh.order_id and oi.order_item_seq_id = osh.order_item_seq_id and oi.ship_group_seq_id = osh.ship_group_seq_id
   LEFT JOIN shipment_status ss on osh.shipment_id = ss.shipment_Id and ss.status_id = 'SHIPMENT_SHIPPED'
-  LEFT JOIN shipment_receipt sr on osh.shipment_id = sr.shipment_id
+  LEFT JOIN shipment_receipt sr on osh.shipment_id = sr.shipment_id and osh.shipment_item_seq_id = sr.shipment_item_seq_id
   LEFT JOIN order_item_ship_group oisg on oh.order_id = oisg.order_id and oi.ship_group_seq_id = oisg.ship_group_seq_id
 
-UNION ALL
-
--- Add manually added items
+-- ******************************************************************
+-- Base Query 2 that fetches Manual Reciept data
+-- ******************************************************************
+WITH manual_receipts AS (
+    SELECT
+        sr.ORDER_ID,
+        sr.PRODUCT_ID,
+        SUM(sr.QUANTITY_ACCEPTED) AS received_qty,
+        MAX(sr.datetime_received) AS received_date
+    FROM SHIPMENT_RECEIPT sr
+    WHERE sr.ORDER_ITEM_SEQ_ID IS NULL
+      AND sr.ORDER_ID IS NOT NULL
+    GROUP BY sr.ORDER_ID, sr.PRODUCT_ID
+)
 SELECT
   oh.order_id as `ORDER_ID`,
   oh.order_name as `ORDER_NAME`,
@@ -122,28 +123,35 @@ FROM
     JOIN order_item_ship_group oisg ON oh.ORDER_ID = oisg.ORDER_ID
 
 -- ******************************************************************
--- SQL to fetch the remaining data, mainly status dates
+-- SQL to fetch the remaining data, mainly item creation status date
 -- ******************************************************************
+with
+  order_status_slice as (
+    select
+      os.order_id,
+      os.order_item_seq_id,
+      os.status_datetime
+    from
+      order_status os
+    where
+      os.status_id = "ITEM_CREATED"
+      and (
+        os.status_datetime between '${min_cursor}' and '${max_cursor}'
+      )
+  )
 select
-    order_id,
-    order_item_seq_id,
-    MIN(os.status_datetime) as ITEM_CREATED_DATE
+  order_id AS `ORDER_ID`,
+  order_item_seq_id AS `ORDER_ITEM_SEQ_ID`,
+  status_datetime AS `ORDER_ITEM_CREATION_DATE`
 from
-    order_status os
-where
-    os.STATUS_ID='ITEM_CREATED'
-    and os.order_id='${ORDER_ID}'
-    and os.ORDER_ITEM_SEQ_ID='${ORDER_ITEM_SEQ_ID}'
-group by
-    os.order_id,
-    os.order_item_seq_id
+  order_status_slice
 
 -- ******************************************************************
 -- Query to combine whole data
 -- ******************************************************************
-
     SELECT o.*, e.*
     FROM original o
     LEFT JOIN enrichment e
     ON o.ORDER_ID = e.ORDER_ID
     and o.ORDER_ITEM_SEQ_ID = e.ORDER_ITEM_SEQ_ID
+
